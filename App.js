bdd6c0b58965a03eaabd8c00cf5f6cffa59645e6
vishitagarg <gargvishita15@gmail.com>
2022-01-06 16:35:23 -0800

    new project eliberary

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import BottomTabNavigator from './components/BottomTabNavigator';
+
+export default class SearchScreen extends Component {
+  render() {
+    return <BottomTabNavigator/>
+  }
+}
